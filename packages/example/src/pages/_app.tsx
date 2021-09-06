3c4a028cb9ab78e2448bfb9526c8605dee66fcf0
Jan Amann <jan@amann.me>
2021-09-06 16:19:33 +0200

    Add eslint to example

--- packages/example/src/pages/_app.tsx
+++ packages/example/src/pages/_app.tsx
@@ -1,5 +1,5 @@
 import {NextQueryParamProvider} from 'next-query-params';
-import type {AppProps} from 'next/app';
+import {AppProps} from 'next/app';
 
 export default function App({Component, pageProps}: AppProps) {
   return (
