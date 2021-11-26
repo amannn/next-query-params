adcb4d5053d820af515824df20085cf4a8499483
Chris Neven <c.k.neven@live.nl>
2021-11-26 12:41:28 +0100

    fix: pass search and hash to both url and as

--- packages/example/src/pages/_app.tsx
+++ packages/example/src/pages/_app.tsx
@@ -1,7 +1,7 @@
-import {NextQueryParamProvider} from 'next-query-params';
-import {AppProps} from 'next/app';
+import { NextQueryParamProvider } from 'next-query-params';
+import { AppProps } from 'next/app';
 
-export default function App({Component, pageProps}: AppProps) {
+export default function App({ Component, pageProps }: AppProps) {
   return (
     <NextQueryParamProvider>
       <Component {...pageProps} />
