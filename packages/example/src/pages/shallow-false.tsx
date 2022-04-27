ecc33a2ec172cb11036432d99fe1e962936b911b
Silviu Ifrim <silviu.redent@gmail.com>
2022-04-27 10:51:59 +0300

    Add `shallow-false` example

---
+++ packages/example/src/pages/shallow-false.tsx
@@ -0,0 +1,47 @@
+import {
+  NextQueryParamProvider,
+  StringParam,
+  useQueryParam,
+  withDefault
+} from 'next-query-params';
+import {ChangeEvent} from 'react';
+
+type ServerSideProps = {
+  query: Record<string, string>;
+};
+
+function ShallowFalsePage(props: ServerSideProps) {
+  const [name, setName] = useQueryParam('name', withDefault(StringParam, ''));
+
+  function onNameInputChange(event: ChangeEvent<HTMLInputElement>) {
+    setName(event.target.value);
+  }
+
+  return (
+    <div>
+      <label>
+        Server side query:
+        <p>{JSON.stringify(props.query)}</p>
+      </label>
+      <label>
+        Name: <input onChange={onNameInputChange} value={name} /> ({name})
+      </label>
+    </div>
+  );
+}
+
+export default function WithParamProvider(props: ServerSideProps) {
+  return (
+    <NextQueryParamProvider shallow={false}>
+      <ShallowFalsePage query={props.query} />
+    </NextQueryParamProvider>
+  );
+}
+
+export async function getServerSideProps(props: ServerSideProps) {
+  return {
+    props: {
+      query: props.query
+    }
+  };
+}
